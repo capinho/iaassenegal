57f4ba45ab3b199bdad6b1409ced7b50740855af
capinho <papadiawara@hotmail.fr>
2022-08-31 02:37:43 +0200

    all bug fixed

--- projsoumare/settings.py
+++ projsoumare/settings.py
@@ -26,7 +26,9 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+#DEBUG = os.getenv('DEBUG')
+DEBUG=False
+
 ALLOWED_HOSTS = ['*']
 
 
